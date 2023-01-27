522282528dd204caea43a1b1df24d9716fafa8e8
jay zade <jaykumarzade@gmail.com>
2023-01-27 17:57:39 +0530

    day4

---
+++ src/Test4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/*Day 4 coding Statement:  Write a program to identify of the a number is positive or negative
+
+Description
+
+Get an input number from the user and check whether it is a positive or negative number.
+*/
+public class Test4 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+
+	}
+
+}
