0d144ef8482b1862f320b40740540f92bb287160
jay zade <jaykumarzade@gmail.com>
2023-01-29 17:56:51 +0530

    half solve day7

--- src/Test7.java
+++ src/Test7.java
@@ -1,8 +1,25 @@
 
+import java.util.Scanner;
+
 public class Test7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the month");
+		int m=Integer.parseInt(sc.nextLine());
+		
+		System.out.println("Enter the year");
+		int y=Integer.parseInt(sc.nextLine());
+		
+		if( ((m==2) && (y%400==0))  )
+		{
+			System.out.println("29 days");
+		}
+		 else if(m==2)
+		{
+			System.out.println("28 days");
+		}
+		
 
 	}
 
