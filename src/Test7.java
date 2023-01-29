583744f36cd58c0773c62dbb04b343868cb5b32a
jay zade <jaykumarzade@gmail.com>
2023-01-29 18:03:16 +0530

    days7 compelete

--- src/Test7.java
+++ src/Test7.java
@@ -1,3 +1,9 @@
+/*
+Day 7 coding Statement:  Write a program to find Number of days in a given month of a given year
+
+Description
+
+Get the number of month and year as input from the user and check the number of days present in that month.*/
 
 import java.util.Scanner;
 
@@ -19,7 +25,17 @@ public class Test7 {
 		{
 			System.out.println("28 days");
 		}
-		
+		 else if(m==4 || m==6 || m==9 || m==11)
+		 {
+			 System.out.println("30 days");
+		 }
+		 else if(m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12)
+		 {
+			 System.out.println("31 days");
+		 }
+		 else{
+			 System.out.println("invalid month");
+		 }
 
 	}
 
