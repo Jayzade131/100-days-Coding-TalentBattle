bee21bcf03e372c2258105b741e60722331e7745
jay zade <jaykumarzade@gmail.com>
2023-02-01 20:30:09 +0530

    complete day10

--- src/Test10.java
+++ src/Test10.java
@@ -1,3 +1,8 @@
+/*Day 10 coding Statement:  Write a program to find Factorial of a number
+
+Description
+
+Get a number from user for which you need to fin the factorial, then calculate the factorial and give it as the output. */
 import java.util.Scanner;
 
 public class Test10 {
@@ -5,8 +10,13 @@ public class Test10 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter the number");
-		int num=
-
-	}
+		int num=Integer.parseInt(sc.nextLine());
+		int fac=1;
+		for(int i=1;i<=num;i++)
+		{
+		fac=fac*i;	
+		}
+		System.out.println("factoiral = "+fac);
+		}
 
 }
