1ff6b9bd98065ce7e765f6b571675558158865c5
jay zade <jaykumarzade@gmail.com>
2023-02-01 20:16:12 +0530

    day 10 start

---
+++ src/Test10.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Test10 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int num=
+
+	}
+
+}
