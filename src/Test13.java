502d7430fcd635b2eee863d0bee45d9b2e620e67
jay zade <jaykumarzade@gmail.com>
2023-02-04 08:40:09 +0530

    day 13 complete

--- src/Test13.java
+++ src/Test13.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 
+/*Day 13 coding Statement:  Write a program to find Sum of N natural numbers
+
+Description
+
+Get the input from the user for the value of n and then find the sum of first n natural numbers.*/
 public class Test13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=sc.nextInt();
+		int sum=0;
+		
+		for(int i=1; i<=n;i++)
+		{
+			sum=sum+i;
+			
+		}
+		System.out.println("sum :"+sum);
 	}
 
 }
