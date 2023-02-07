39aba1c8bb1b7e5483961c86c704d41c9afb9eff
jay zade <jaykumarzade@gmail.com>
2023-02-07 13:00:55 +0530

    day 16 complete

--- src/Test16.java
+++ src/Test16.java
@@ -1,9 +1,34 @@
+/*Day 16 coding Statement : Write a program to identify if the number is Perfect number or not
+
+Description
+
+Get the input from the user and check whether that number is a perfect number or not.*/
+import java.util.Scanner;
 
 public class Test16 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			Scanner sc=new Scanner(System.in);
+			int sum=0;
+			System.out.println("enter the number");
+			int num=sc.nextInt();
+			
+			for(int i=1;i<num;i++)
+			{
+				if(num%i==0)
+				{
+					
+					sum=sum+i;
+				}
+			}
+			
+		if(sum==num){
+			System.out.println("perfect number");
+		}
+		else{
+			System.out.println("not perfect number");
+		}
+  
 	}
 
 }
