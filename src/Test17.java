ab9fe5f506113d7a613ac2dcf72a22731470fbdb
jay zade <jaykumarzade@gmail.com>
2023-02-08 19:49:50 +0530

    update

--- src/Test17.java
+++ src/Test17.java
@@ -1,8 +1,24 @@
+/*Day 17 coding Statement : Write a program to find the Factors of a number
+
+Description
+
+Get an input from the user and find the factors of the number.*/
+import java.util.Scanner;
 
 public class Test17 {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int num=sc.nextInt();
 		
+		for(int i=1;i<=num;i++)
+		{
+			if(num%i==0)
+			{
+				System.out.println(i);
+			}
+		}
 
 	}
 
