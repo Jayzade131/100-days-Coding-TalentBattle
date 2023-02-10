a0ff44cce334e8c0c8b97b8f5e3e331f0821c30a
jay zade <jaykumarzade@gmail.com>
2023-02-10 15:26:56 +0530

    update day19

---
+++ src/Test19.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Test19 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter the number");
+		int num = sc.nextInt(); 
+		 int copy = num;
+		 int result=0;
+		 int n=0;
+		 int rem;
+		 while (num != 0)
+		 {
+		 num /= 10;
+		 n++;
+		 }
+		 num = copy; 
+		 while (num != 0) 
+		 {
+		 rem = num%10;
+		 int mul=1;
+		 for(int i=1;i<=n;i++) {
+		 mul=mul*rem; 
+		 } 
+		 result=result+mul;
+		 num /= 10; 
+		 }
+		 if(result==copy)
+		
+		 System.out.println("Armstrong Number");
+		 else
+		 System.out.println("Not Armstrong number");
+
+
+	}
+
+}
